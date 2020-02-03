46ad901dd0775bf8eca08b18a3545f789a65b92b
SatoGaito <gaito04024570@gmail.com>
2020-02-03 07:49:31 +0000

    [Finish] EventShowLayout

--- app/controllers/event_comments_controller.rb
+++ app/controllers/event_comments_controller.rb
@@ -9,6 +9,12 @@ class EventCommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = EventComment.find_by(id: params[:id], event_id: params[:event_id])
+    comment.delete
+    redirect_to event_path(comment.event_id)
+  end
+
   private
   def event_comment_params
     params.require(:event_comment).permit(:comment, :user_id, :event_id)
