92dba29713e66a828376c236a10891a14ea514f1
SatoGaito <gaito04024570@gmail.com>
2020-02-05 19:06:24 +0000

    [Modify] EventImage

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_one_attached :p_image
   attachment :profile_image
   has_many :event_comments, dependent: :destroy
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :name, :nickname, :college, presence: true, length: { minimum: 2, maximum: 10 }, on: :update
   validates :grade, :gender, presence: true, on: :update
