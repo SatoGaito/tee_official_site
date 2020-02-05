c2cb45cee1a187ee79ad2d528c45e2e1d306d5e2
SatoGaito <gaito04024570@gmail.com>
2020-02-05 19:21:57 +0000

    [Modify] EventImage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @events = Event.all
+    @events = Event.all.order(date: :desc)
   end
 
   def new
