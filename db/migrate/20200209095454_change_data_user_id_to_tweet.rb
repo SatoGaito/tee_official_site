1959de20c6a45233f35f63ba0219852d145e5af0
SatoGaito <gaito04024570@gmail.com>
2020-02-12 20:39:25 +0000

    [Modify] migration

--- db/migrate/20200209095454_change_data_user_id_to_tweet.rb
+++ db/migrate/20200209095454_change_data_user_id_to_tweet.rb
@@ -1,5 +1,5 @@
 class ChangeDataUserIdToTweet < ActiveRecord::Migration[5.2]
   def change
-    change_column :tweets, :user_id, :integer
+    change_column :tweets, :user_id, :integer USING CAST(user_id AS integer)
   end
 end
