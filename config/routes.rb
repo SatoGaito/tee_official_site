5f611b0fbf74774c0b5c5340333f52bbd3b90be7
SatoGaito <gaito04024570@gmail.com>
2020-02-16 16:01:05 +0000

    TimeOut

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users(:users,
+  devise_for (:users,
     controllers: {
       registrations: 'users/registrations',
       passwords: 'users/passwords'
