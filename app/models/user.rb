0dd7794bcce98001f81b28a8b9f1548b20052afa
SatoGaito <gaito04024570@gmail.com>
2020-02-18 09:57:52 +0000

    [Modify] timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_one_attached :p_image
   has_many :event_comments, dependent: :destroy
